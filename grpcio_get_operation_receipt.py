7ab8d73f63820c8ae18257d9304b2ace59e98e09
Sclea3 <sashamaty30@gmail.com>
2025-12-09 21:56:36 +0400

    Практикуемся в работе с grpcio. Практикуемся в работе с grpcio: создание операции покупки

---
+++ grpcio_get_operation_receipt.py
@@ -0,0 +1,48 @@
+import grpc
+
+from contracts.services.gateway.users.rpc_create_user_pb2 import CreateUserRequest, CreateUserResponse
+from contracts.services.gateway.users.users_gateway_service_pb2_grpc import UsersGatewayServiceStub
+
+from contracts.services.gateway.accounts.accounts_gateway_service_pb2_grpc import AccountsGatewayServiceStub
+from contracts.services.gateway.accounts.rpc_open_debit_card_account_pb2 import (OpenDebitCardAccountRequest,
+                                                                                 OpenDebitCardAccountResponse)
+
+from contracts.services.gateway.operations.operations_gateway_service_pb2_grpc import OperationsGatewayServiceStub
+from contracts.services.gateway.operations.rpc_make_top_up_operation_pb2 import (MakeTopUpOperationRequest,
+                                                                                 MakeTopUpOperationResponse)
+from contracts.services.operations.operation_pb2 import OperationStatus
+
+from tools.fakers import fake
+
+channel = grpc.insecure_channel("localhost:9003")
+
+users_gateway_service = UsersGatewayServiceStub(channel)
+accounts_gateway_service = AccountsGatewayServiceStub(channel)
+operations_gateway_service = OperationsGatewayServiceStub(channel)
+
+create_user_request = CreateUserRequest(
+    email=fake.email(),
+    last_name=fake.last_name(),
+    first_name=fake.first_name(),
+    middle_name=fake.middle_name(),
+    phone_number=fake.phone_number()
+)
+
+create_user_response: CreateUserResponse = users_gateway_service.CreateUser(create_user_request)
+print("Create user response:", create_user_response)
+
+open_debit_card_request = OpenDebitCardAccountRequest(
+    user_id=create_user_response.user.id,
+)
+open_debit_card_response: OpenDebitCardAccountResponse = accounts_gateway_service.OpenDebitCardAccount(
+    open_debit_card_request)
+print("Open credit card account response:", open_debit_card_response)
+
+make_top_up_request = MakeTopUpOperationRequest(
+    status=OperationStatus.OPERATION_STATUS_COMPLETED,
+    amount=fake.amount(),
+    card_id=open_debit_card_response.account.cards[0].id,
+    account_id=open_debit_card_response.account.id,
+)
+make_top_up_response = operations_gateway_service.MakeTopUpOperation(make_top_up_request)
+print("Make purchase operation response:", make_top_up_response)
