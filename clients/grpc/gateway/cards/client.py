587387dc7a864659c6e780850c753f2e31e8cfd3
Sclea3 <sashamaty30@gmail.com>
2025-12-10 00:35:54 +0400

    Знакомство с gRPC API клиентами. Практика: Написание gRPC API клиента

---
+++ clients/grpc/gateway/cards/client.py
@@ -0,0 +1,85 @@
+from grpc import Channel
+
+from clients.grpc.client import GRPCClient
+from clients.grpc.gateway.client import build_gateway_grpc_client
+from contracts.services.gateway.cards.cards_gateway_service_pb2_grpc import CardsGatewayServiceStub
+from contracts.services.gateway.cards.rpc_issue_virtual_card_pb2 import IssueVirtualCardRequest, \
+    IssueVirtualCardResponse
+from contracts.services.gateway.cards.rpc_issue_physical_card_pb2 import IssuePhysicalCardRequest, \
+    IssuePhysicalCardResponse
+
+
+class CardsGatewayGRPCClient(GRPCClient):
+    """
+    gRPC-клиент для взаимодействия с CardsGatewayService.
+    Предоставляет высокоуровневые методы для выпуска виртуальных и физических карт.
+    """
+
+    def __init__(self, channel: Channel):
+        """
+        Инициализация клиента с указанным gRPC-каналом.
+
+        :param channel: gRPC-канал для подключения к CardsGatewayService.
+        """
+        super().__init__(channel)
+        self.stub = CardsGatewayServiceStub(channel)
+
+    def issue_virtual_card_api(self, request: IssueVirtualCardRequest) -> IssueVirtualCardResponse:
+        """
+        Низкоуровневый вызов метода IssueVirtualCard через gRPC.
+        Выполняет прямой вызов CardsGatewayService.IssueVirtualCard.
+
+        :param request: gRPC-запрос с данными для выпуска виртуальной карты.
+        :return: Ответ от сервиса с данными выпущенной виртуальной карты.
+        """
+        return self.stub.IssueVirtualCard(request)
+
+    def issue_physical_card_api(self, request: IssuePhysicalCardRequest) -> IssuePhysicalCardResponse:
+        """
+        Низкоуровневый вызов метода IssuePhysicalCard через gRPC.
+        Выполняет прямой вызов CardsGatewayService.IssuePhysicalCard.
+
+        :param request: gRPC-запрос с данными для выпуска физической карты.
+        :return: Ответ от сервиса с данными выпущенной физической карты.
+        """
+        return self.stub.IssuePhysicalCard(request)
+
+    def issue_virtual_card(self, user_id: str, account_id: str) -> IssueVirtualCardResponse:
+        """
+        Выпуск виртуальной карты для указанного пользователя и счета.
+        Высокоуровневый метод-обёртка для упрощения работы с API.
+
+        :param user_id: Идентификатор пользователя, для которого выпускается карта.
+        :param account_id: Идентификатор счета, к которому привязывается карта.
+        :return: Ответ с информацией о выпущенной виртуальной карте.
+        """
+        request = IssueVirtualCardRequest(
+            user_id=user_id,
+            account_id=account_id
+        )
+        return self.issue_virtual_card_api(request)
+
+    def issue_physical_card(self, user_id: str, account_id: str) -> IssuePhysicalCardResponse:
+        """
+        Выпуск физической карты для указанного пользователя и счета.
+        Высокоуровневый метод-обёртка для упрощения работы с API.
+
+        :param user_id: Идентификатор пользователя, для которого выпускается карта.
+        :param account_id: Идентификатор счета, к которому привязывается карта.
+        :return: Ответ с информацией о выпущенной физической карте.
+        """
+        request = IssuePhysicalCardRequest(
+            user_id=user_id,
+            account_id=account_id
+        )
+        return self.issue_physical_card_api(request)
+
+
+def build_cards_gateway_grpc_client() -> CardsGatewayGRPCClient:
+    """
+    Фабрика для создания экземпляра CardsGatewayGRPCClient.
+    Создаёт gRPC-канал и возвращает готовый к использованию клиент.
+
+    :return: Инициализированный клиент для CardsGatewayService.
+    """
+    return CardsGatewayGRPCClient(channel=build_gateway_grpc_client())
